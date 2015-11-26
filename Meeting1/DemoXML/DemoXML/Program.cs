66d55e95ec60556267bd0a91b2dc9f805a63b734
Tiberiu Covaci <tibi@covaci.se>
2015-11-26 18:49:30 +0000

    First Meeting

---
+++ Meeting1/DemoXML/DemoXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMethod(1, "2");
+            Console.WriteLine("Hello World");
+        }
+        /// <summary>
+        /// This is MY Method
+        /// </summary>
+        /// <param name="param1">This is the first parameter</param>
+        /// <param name="param2">The second parameter</param>
+        /// <returns>Zeros</returns>
+        static int MyMethod(int param1, string param2)
+        {
+            return 0;
+        }
+    }
+}
